11fa69696be67c61e5b7af9f655e7daa6431c760
SVladimir <semennikovvladimir@gmail.com>
2020-11-24 16:57:10 +0300

    Add Task1 solution

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -1,7 +1,11 @@
 package day0;
 
+import javax.sound.midi.Soundbank;
+
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");;
+        System.out.println("Hello World");
+        int x = 21;
+        System.out.println("Число "+x);
     }
 }
